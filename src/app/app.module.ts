8da0a86ad100e3727cd5268f4e98ce148c1a3b8d
Gabriel Canderoli <gabyc17@live.com.ar>
2017-03-03 14:36:17 -0300

    fixed base href, cleaning code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './user.component/user.component';
 import { Conector }         from './conector'; //servicio propio
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
